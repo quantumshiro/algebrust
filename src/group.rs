17f54698cd0f78b25b22613fe9c8c902ff9b38f3
quantumshiro <hiro.1nakanishi@gmail.com>
2024-05-20 20:29:13 +0900

    fix: group type

--- src/group.rs
+++ src/group.rs
@@ -1,46 +1,30 @@
-trait Group {
-    fn op(&self, other: &Self) -> Self;
-    fn identity() -> Self;
-    fn inverse(&self) -> Self;
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
+pub struct Group<T> {
+    element: T,
 }
 
-impl Group for i32 {
-    // The operation of the group is addition.
-    fn op(&self, other: &Self) -> Self {
-        self + other
-    }
+impl Add<Group<i32>> for Group<i32> {
+    type Output = Group<i32>;
 
-    // The identity element of the group is 0.
-    fn identity() -> Self {
-        0
-    }
-
-    // The inverse of an element is its negation.
-    fn inverse(&self) -> Self {
-        -self
+    fn add(self, other: Group<i32>) -> Group<i32> {
+        Group {
+            element: self.element + other.element,
+        }
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::group;
     use super::*;
 
     #[test]
-    fn test_group() {
-        let a: i32 = 5;
-        let b: i32 = -3;
-        let c: i32 = 2;
+    fn test_group_op() {
+        let g1 = Group { element: 1 };
+        let g2 = Group { element: 2 };
 
-        // Closure property: a + b should be an integer
-        assert_eq!(a.op(&b), a + b);
-
-        // Associativity: (a + b) + c should be equal to a + (b + c)
-        assert_eq!(a.op(&b).op(&c), a.op(&b.op(&c)));
-
-        // Identity: a + 0 should be equal to a
-        assert_eq!(a.op(&i32::identity()), a);
-
-        // Inverse: a + (-a) should be equal to 0
-        assert_eq!(a.op(&a.inverse()), i32::identity());
+        assert_eq!(g1 + g2, group::Group { element: 3 });
     }
 }
